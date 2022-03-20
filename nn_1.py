8c0391fa5ce7db25538c9d72b67eff175d49b538
Theodoros Giannakopoulos <tyiannak@gmail.com>
2022-03-20 22:57:05 +0200

    evaluation added

--- nn_1.py
+++ nn_1.py
@@ -2,9 +2,20 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 from torch.autograd import Variable
-
 import numpy as np
-n_samples_per_class = 1000
+from sklearn.metrics import f1_score
+
+
+def evaluate_model(n_model, test_data, test_labels):
+    X_test_var = Variable(torch.FloatTensor(test_data), requires_grad=False) 
+    with torch.no_grad():
+        test_result = n_model(X_test_var)
+    values, labels = torch.max(test_result, 1)
+    y_pred = labels.data.numpy()
+    return f1_score(y_pred, test_labels)
+
+
+n_samples_per_class = 200
 indepedence_rate = 0.5
 dimensions = 20
 
@@ -27,7 +38,6 @@ y_train = y[::2]
 X_test = X[1::2, :]
 y_test = y[1::2]
 
-#thank you very much https://www.kaggle.com/mburakergenc/ttianic-minimal-pytorch-mlp
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
@@ -51,13 +61,13 @@ criterion = nn.CrossEntropyLoss()
 
 optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
 
-batch_size = 64
+batch_size = 32
 n_epochs = 500
 batch_no = len(X_train) // batch_size
 
-train_loss = 0
 train_loss_min = np.Inf
 for epoch in range(n_epochs):
+    train_loss = 0  # should be this initalized here?
     for i in range(batch_no):
         start = i * batch_size
         end   = start + batch_size
@@ -66,12 +76,12 @@ for epoch in range(n_epochs):
         
         optimizer.zero_grad()
         output = model(x_var)
-        loss   = criterion(output,y_var)
+        loss   = criterion(output, y_var)
         loss.backward()
         optimizer.step()
         
         values, labels = torch.max(output, 1)
-        num_right   = np.sum(labels.data.numpy() == y_train[start:end])
+        f1_train = f1_score(labels.data.numpy(), y_train[start:end])
         train_loss += loss.item()*batch_size
     
     train_loss = train_loss / len(X_train)
@@ -80,15 +90,9 @@ for epoch in range(n_epochs):
         torch.save(model.state_dict(), "model.pt")
         train_loss_min = train_loss
     
-    if epoch % 200 == 0:
+    if epoch % 20 == 0:
         print('')
-        print("Epoch: {} \tTrain Loss: {} \tTrain Accuracy: {}".format(epoch+1, train_loss,num_right / len(y_train[start:end]) ))
+        f1_test = evaluate_model(model, X_test, y_test)
+        print("Epoch: {} \tTrain Loss: {} \tTrain Accuracy: {} \tTest Accuracy: {}".format(epoch+1, train_loss, f1_train, f1_test))
 print('Training Ended! ')
 
-
-X_test_var = Variable(torch.FloatTensor(X_test), requires_grad=False) 
-with torch.no_grad():
-    test_result = model(X_test_var)
-values, labels = torch.max(test_result, 1)
-survived = labels.data.numpy()
-print(survived)
