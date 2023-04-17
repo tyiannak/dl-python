e636fd028eae117c7fc6ac745aa2daf8dca87808
Theodoros Giannakopoulos <tyiannak@gmail.com>
2023-04-17 13:33:21 +0300

    preprocess

--- text_bias_example.py
+++ text_bias_example.py
@@ -1,13 +1,14 @@
 import gensim
 from gensim.models import Word2Vec
 from nltk.tokenize import sent_tokenize, word_tokenize
-
+import re
 
 def train_word2vec(file_name):
     sample = open(file_name)
     s = sample.read()
     # pre_process text:
     f = s.replace("\n", " ")
+    f = re.sub('[^A-Za-z0-9.]+', ' ', f)
     data = []
     # iterate through each sentence in the file
     for i in sent_tokenize(f):
@@ -23,12 +24,13 @@ def train_word2vec(file_name):
                                    window=5, sg=1)
     model.save(file_name + ".model")
     print(file_name + ".model")
+    print(len(model.wv))
     return model
 
 
-data_name = "books_small.txt"
+data_name = "books.txt"
 
-if 1:
+if 0:
     model1 = train_word2vec(data_name)
 else:
     model1 = Word2Vec.load(data_name + ".model") 
@@ -44,5 +46,5 @@ print(f"Cosine similarity between {w1} and {w4}: {model1.wv.similarity(w1, w4)}"
 print(f"Cosine similarity between {w2} and {w4}: {model1.wv.similarity(w2, w4)}")
 
 vector = model1.wv['woman']
-sims = model1.wv.most_similar('woman', topn=20)
+sims = model1.wv.most_similar('man', topn=20)
 print(sims)
